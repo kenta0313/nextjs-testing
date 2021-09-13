ea5503528a6aca3ff5b7bb8faa6b1108ccb444ed
kenta0313 <asanokenta@me.com>
2021-09-13 18:41:12 +0900

    🎉

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
+      Hello Next.js
+    </div>
+  )
+}
+export default Home
