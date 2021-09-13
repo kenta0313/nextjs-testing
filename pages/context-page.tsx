00aa741f205570686dcc343f1e13fb3b17cfea7c
kenta0313 <asanokenta@me.com>
2021-09-13 19:20:17 +0900

    ✅ page navigation　のテスト追加

---
+++ pages/context-page.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const ContextPage: React.FC = () => {
+  return (
+    <Layout title="context">
+      <p className="text-4xl mb-10">context page</p>
+    </Layout>
+  )
+}
+
+export default ContextPage;
---
+++ pages/task-page.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const TaskPage: React.FC = () => {
+  return (
+    <Layout title="Task">
+      <p className="text-4xl mb-10">todos page</p>
+    </Layout>
+  )
+}
+
+export default TaskPage;
